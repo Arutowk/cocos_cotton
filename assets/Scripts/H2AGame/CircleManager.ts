7c3d8a193b80851694b098cad65e1ce2d67b4b7d
sli97 <775303361@qq.com>
2022-04-06 12:48:51 +0800

    init commit assets

--- assets/Scripts/H2AGame/CircleManager.ts
+++
@@ -1,8 +0,0 @@
-import {Component, _decorator} from "cc";
-
-const {ccclass, property} = _decorator;
-
-@ccclass('CircleManager')
-export class CircleManager extends Component {
-    index: number
-}
