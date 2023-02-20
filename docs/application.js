56cb61caa4e56c81a38d20b91e9fc23c5bbc1d98
Arutowk <wwk199375@gmail.com>
2023-02-20 12:17:43 +0800

    重新打包

--- docs/application.js
+++ docs/application.js
@@ -96,7 +96,7 @@ System.register([], function (_export, _context) {
     }
 
     function loadSettingsJson(cc) {
-      var settings = 'src/settings.47bfc.json';
+      var settings = 'src/settings.json';
       return new Promise(function (resolve, reject) {
         if (typeof fsUtils !== 'undefined' && !settings.startsWith('http')) {
           var result = fsUtils.readJsonSync(settings);
